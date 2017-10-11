1415d19cb413f929e9aab1f7c5926d81f966ad6b
gabe cemaj <gabriel@cemaj.com>
2017-10-11 13:31:33 -0400

    parse xml

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from gigaword_api import Gigaword
 
 def main():
-    giga = Gigaword('/home/rldata/gigaqord/data')
+    giga = Gigaword('/home/rldata/gigaword/data')
     print(giga.corpora)
     print(giga.documents)
     giga.getFile(giga.corpora[0],0)
