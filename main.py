1fa1ec955631c576ccd512e528f193c018cac307
gabox8888 <gabriel@cemaj.com>
2017-10-14 14:32:23 -0400

    testing distributions

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from gigaword_api import Gigaword
 
 def printToFile(name,sentences):
-    f=open(name,'w')
-    for s in sentences:
-        f.write(s+'\n')
-    f.close()
+    with open(name, "w",encoding='utf-8') as file:
+        file.writelines( "%s\n" % item for item in sentences )
 
 def main():
     giga = Gigaword('/home/rldata/gigaword/data')
     sentences = giga.getSentencesFromKDocs((0.7,0.2,0.1),10)
-    printToFile('./trainGigaEnglish.text',sentences[0])
-    printToFile('./valGigaEnglish.text',sentences[1])
-    printToFile('./testGigaEnglish.text',sentences[2])
+    printToFile('../trainGigaEnglish.text',sentences[0])
+    printToFile('../valGigaEnglish.text',sentences[1])
+    printToFile('../testGigaEnglish.text',sentences[2])
 
 if __name__ == '__main__': main()
